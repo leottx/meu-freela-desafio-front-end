bab76d7c0b00dd0fd0598a9476753c74be0dc475
Leonardo <tteixeira.leonardo@gmail.com>
2021-12-20 14:24:46 -0300

    refactor(refactor input field): refatora o componente contactfield
    altera o estilo da borda quando o foco do teclado estiver sobre o campo

--- src/components/ContactField.js
+++ src/components/ContactField.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import { darken } from 'polished';
 import styled from 'styled-components';
 
+// COMPONENTES
 const ContactField = styled.input.attrs({ type: 'text' })`
   font-size: 1.8rem;
   border-radius: 8px;
   border: 1px solid ${({ theme: { c } }) => c.grayLight};
   background: ${({ theme: { c } }) => c.white};
-  padding: 1.6rem;
+  height: 5.6rem;
+  padding: 0 1.6rem;
+  display: block;
   outline: none;
   &::placeholder {
     color: ${({ theme: { c } }) => c.gray};
   }
   &:focus {
-    border-color: ${({ theme: { c } }) => c.red};
+    border-color: ${({ theme: { c } }) => darken(0.08, c.gray)};
+    border-width: 2px;
   }
 `;
 
