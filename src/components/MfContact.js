febef0cf9431ee0985d7af16ddff079a948a095b
Leonardo <tteixeira.leonardo@gmail.com>
2021-12-20 14:23:05 -0300

    refactor(refact contact component): refatora o componente mfcontact
    refatora o componente mfcontact para que esteja responsivo para todas as telas

--- src/components/MfContact.js
+++ src/components/MfContact.js
@@ -1,59 +1,76 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import styled from 'styled-components';
+
+// UTILIDADES
 import { lighten } from 'polished';
 
 // COMPONENTES
 import WrapperMain from '@Components/WrapperMain';
 import { SectionTitle, SectionText } from '@Components/SectionText';
 import Button from '@Components/Button';
-
-import ContactField from './ContactField';
+import ContactField from '@Components/ContactField';
 
 const MfAboutStyled = styled.section`
   background: ${({ theme: { c } }) => c.grayLightest};
   color: ${({ theme: { c } }) => c.grayDark};
-  padding: 7.4rem 0 8rem;
+  padding: 3.6rem 0 5.2rem;
   > div {
     padding: 0 2.4rem;
   }
   h2 {
-    margin-bottom: 2rem;
+    margin-bottom: 3rem;
   }
   p {
     margin-bottom: 3.6rem;
   }
   form {
     display: grid;
-    grid: auto auto / repeat(3, 1fr);
+    gap: 2rem;
     grid-template-areas:
-      'lname lnumber -'
-      'iname inumber button';
-    gap: 3px 2.2rem;
+      'name'
+      'number'
+      'button';
+    > div:first-child {
+      grid-area: name;
+    }
+    > div:last-child {
+      grid-area: number;
+    }
   }
   label {
     font-size: 1.4rem;
     font-weight: 700;
-    &:first-of-type {
-      grid-area: lname;
-    }
-    &:last-of-type {
-      grid-area: lnumber;
-    }
+    display: block;
+    margin-bottom: 3px;
   }
   input {
-    &:first-of-type {
-      grid-area: iname;
-    }
-    &:last-of-type {
-      grid-area: inumber;
-    }
+    width: 100%;
   }
   button {
     grid-area: button;
-    padding: ${1.6 / 2.2}em 2rem;
+    height: 5.6rem;
     background: ${({ theme: { c } }) => c.green};
     &:hover {
       background-color: ${({ theme: { c } }) => lighten(0.06, c.green)};
+    }
+  }
+
+  @media only screen and (min-width: 768px) {
+    padding: 7.4rem 0 8rem;
+    form {
+      grid-template-areas:
+        'name number'
+        'button button';
+    }
+  }
+
+  @media only screen and (min-width: 992px) {
+    form {
+      grid-template-columns: repeat(3, 1fr);
+      grid-template-areas: 'name number button';
+      button {
+        align-self: end;
+      }
     }
   }
 `;
@@ -67,10 +84,14 @@ const MfContact = () => {
           Preencha seus dados para que a gente possa entrar em contato.
         </SectionText>
         <form>
-          <label>Nome Completo</label>
-          <ContactField placeholder='Ex: Mateus Ávila Isidoro' />
-          <label>Whatsapp</label>
-          <ContactField placeholder='(99) 99999-9999' />
+          <div>
+            <label>Nome Completo</label>
+            <ContactField placeholder='Ex: Mateus Ávila Isidoro' />
+          </div>
+          <div>
+            <label>Whatsapp</label>
+            <ContactField placeholder='(99) 99999-9999' />
+          </div>
           <Button>Peça uma reunião</Button>
         </form>
       </WrapperMain>
