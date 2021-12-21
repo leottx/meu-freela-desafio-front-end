136bc6eb144e190926c32c8e215035f2933e57bb
Leonardo <tteixeira.leonardo@gmail.com>
2021-12-20 22:42:51 -0300

    feat(form validation): valida os formulario
    valida os campos de nome e numero de telefone
    
    issue #16

--- src/components/ContactField.js
+++ src/components/ContactField.js
@@ -5,7 +5,6 @@ import styled from 'styled-components';
 const ContactField = styled.input.attrs({ type: 'text' })`
   font-size: 1.8rem;
   border-radius: 8px;
-  border: 1px solid ${({ theme: { c } }) => c.grayLight};
   background: ${({ theme: { c } }) => c.white};
   height: 5.6rem;
   padding: 0 1.6rem;
@@ -18,6 +17,10 @@ const ContactField = styled.input.attrs({ type: 'text' })`
     border-color: ${({ theme: { c } }) => darken(0.08, c.gray)};
     border-width: 2px;
   }
+  ${({ isThisValid, isFormValid, theme: { c } }) =>
+    isThisValid === false && isFormValid === false
+      ? `border: 2px solid ${c.red};`
+      : `border: 1px solid ${c.gray};`}
 `;
 
 export default ContactField;
