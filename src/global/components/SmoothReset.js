a30b880d3860a7aba7f0bca470984c5286ca84b9
Leonardo <tteixeira.leonardo@gmail.com>
2021-12-20 22:34:43 -0300

    refactor(scroll behavior): adiciona o comportamento do scroll que estava faltando

--- src/global/components/SmoothReset.js
+++ src/global/components/SmoothReset.js
@@ -1,8 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
 
+// COMPONENTES
 const SmoothReset = createGlobalStyle`
   html {
     font-size: 62.5%;
+    scroll-behavior: smooth;
   }
   *,
   *::before,
